14f982a6527620e97b2bca764d243734688710b4
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2014-12-25 10:00:16 -0600

    * Initial Add

---
+++ MakeItSo/MakeItSo.h
@@ -0,0 +1,19 @@
+//
+//  MakeItSo.h
+//  MakeItSo
+//
+//  Created by Todd Ditchendorf on 12/25/14.
+//  Copyright (c) 2014 Todd Ditchendorf. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MakeItSo.
+FOUNDATION_EXPORT double MakeItSoVersionNumber;
+
+//! Project version string for MakeItSo.
+FOUNDATION_EXPORT const unsigned char MakeItSoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MakeItSo/PublicHeader.h>
+
+
