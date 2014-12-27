8720c006379b40f4d4f487e7cc06f4884af8e1af
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2014-12-27 10:57:26 -0600

    * sql gen

--- MISGenerator/MISField.h
+++ MISGenerator/MISField.h
@@ -8,11 +8,22 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(NSInteger, MISFieldSqlType) {
+    MISFieldSqlTypeInvalid = -1,
+    MISFieldSqlTypeNull = 0,
+    MISFieldSqlTypeString,
+    MISFieldSqlTypeNumber,
+    MISFieldSqlTypeDate,
+    MISFieldSqlTypeData,
+};
+
 @interface MISField : NSObject
 @property (nonatomic, copy) NSString *sourceString;
 @property (nonatomic, copy) NSString *name;
 @property (nonatomic, copy) NSString *type;
 
+@property (nonatomic, assign) MISFieldSqlType sqlType;
+@property (nonatomic, copy) NSString *sqlTypeString;
 @property (nonatomic, assign) BOOL isPrimaryKey;
 @property (nonatomic, assign) BOOL isForeignKey;
 @end
--- MISGenerator/MISField.m
+++ MISGenerator/MISField.m
@@ -10,6 +10,15 @@
 
 @implementation MISField
 
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.sqlType = MISFieldSqlTypeInvalid;
+    }
+    return self;
+}
+
+
 - (void)dealloc {
     self.sourceString = nil;
     self.name = nil;
@@ -22,5 +31,55 @@
     return [NSString stringWithFormat:@"<%@ %p %@>", [self class], self, self.name];
 }
 
+
+- (MISFieldSqlType)sqlType {
+    if (MISFieldSqlTypeInvalid == _sqlType) {
+        MISFieldSqlType type = MISFieldSqlTypeInvalid;
+        
+        if ([self.type hasPrefix:@"NSString"]) {
+            type = MISFieldSqlTypeString;
+        } else if ([self.type hasPrefix:@"NSNumber"]) {
+            type = MISFieldSqlTypeNumber;
+        } else if ([self.type hasPrefix:@"NSDate"]) {
+            type = MISFieldSqlTypeDate;
+        } else if ([self.type hasPrefix:@"NSData"]) {
+            type = MISFieldSqlTypeData;
+        } else {
+            TDAssert(0);
+        }
+        
+        _sqlType = type;
+    }
+    TDAssert(MISFieldSqlTypeInvalid != _sqlType);
+    return _sqlType;
+}
+
+
+- (NSString *)sqlTypeString {
+    NSString *str = nil;
+    
+    switch (self.sqlType) {
+        case MISFieldSqlTypeNull:
+            str = @"null";
+            break;
+        case MISFieldSqlTypeString:
+            str = @"string";
+            break;
+        case MISFieldSqlTypeNumber:
+            str = @"int";
+            break;
+        case MISFieldSqlTypeDate:
+            str = @"date";
+            break;
+        case MISFieldSqlTypeData:
+            str = @"blob";
+            break;
+        default:
+            TDAssert(0);
+            break;
+    }
+    
+    return str;
+}
 
 @end
