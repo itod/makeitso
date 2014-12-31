94f0d8f7c6aa56b1a236ff361224f2a459e22d02
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2014-12-31 09:44:48 -0600

    * tests

---
+++ MakeItSoTests/MISKeyGeneratorTestCase.m
@@ -0,0 +1,46 @@
+//
+//  MISTestCase.m
+//  MakeItSoTests
+//
+//  Created by Todd Ditchendorf on 12/25/14.
+//  Copyright (c) 2014 Todd Ditchendorf. All rights reserved.
+//
+
+#import "MISBaseTestCase.h"
+#import "MISKeyGenerator.h"
+
+@interface MISKeyGeneratorTestCase : MISBaseTestCase
+@property (nonatomic, retain) MISKeyGenerator *generator;
+@end
+
+@implementation MISKeyGeneratorTestCase
+
+- (void)dealloc {
+    self.generator = nil;
+    [super dealloc];
+}
+
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
