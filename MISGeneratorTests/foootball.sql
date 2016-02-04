deb3f78d06a3fb2bd7aa30666fbd003c97c6b7b4
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2016-02-03 22:58:34 -0600

    * one to many sql

--- MISGeneratorTests/foootball.sql
+++ MISGeneratorTests/foootball.sql
@@ -6,10 +6,17 @@ CREATE TABLE keys (
 );
 INSERT INTO keys (nextID, name) VALUES (0, 'global');
 
-
 CREATE TABLE team (
     objectID INTEGER PRIMARY KEY NOT NULL,
     name TEXT
+);
+
+CREATE TABLE team_player (
+    rowID INTEGER PRIMARY KEY NOT NULL,
+    ownerID INTEGER NOT NULL,
+    memberID INTEGER NOT NULL,
+    FOREIGN KEY(ownerID) REFERENCES team(objectID),
+    FOREIGN KEY(memberID) REFERENCES player(objectID)
 );
 
 CREATE TABLE player (
